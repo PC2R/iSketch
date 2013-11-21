8feb9f58342ec2b05cca0a8e2f51215c3579c1a6
fleurdeyeux <fleurdeyeux@live.fr>
2013-11-21 18:22:26 +0100

    Update documentation.

--- Server/server.mli
+++ Server/server.mli
@@ -6,8 +6,8 @@
 val max_players : int ref
 
 (** This class sets up the server
- @param port listening port number
-*)
+@param port listening port number
+ *)
 class server :
 int ->
 int ->
@@ -24,3 +24,25 @@ object
   method start_rounds : unit -> unit
 end
 
+(** This class sets up one player
+@param sd socket descriptor
+@param sa socket address
+ *)
+class player :
+Unix.file_descr ->
+Unix.sockaddr ->
+object
+  val s_descr : Unix.file_descr
+  val s_addr : Unix.sockaddr
+  val mutable pseudo : string
+  val mutable number : int
+  method start : unit -> unit
+  method stop : unit -> unit
+  method connection_player : unit -> unit
+  method set_number_and_pseudo : int ref -> string -> unit
+  method send_roles_and_word : unit -> unit
+  method wait_word_proposition : unit -> unit
+  method send_word_proposition : unit -> unit
+  method wait_drawing_proposition : unit -> unit
+  method send_drawing_proposition : unit -> unit
+end
