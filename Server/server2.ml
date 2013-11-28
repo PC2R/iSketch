267dc0184a7f2ed954a448ecf3c9a14209817d62
fleurdeyeux <fleurdeyeux@live.fr>
2013-11-28 15:55:29 +0100

    Update : add function for dictionary initialization

--- Server/server2.ml
+++ Server/server2.ml
@@ -17,10 +17,14 @@ let max_players = ref 4
 let timeout = ref 30
 let port = ref 2013
 let verbose_mode = ref false
+
 let dictionary_filename = ref "dictionary"
+let logfile = "log/server.log"
+
+let dictionary_words = ref (Array.make 0 "")
 
 let trace message =
-  let out_channel = open_out_gen [Open_append;Open_creat] 0o666 "log/server.log"
+  let out_channel = open_out_gen [Open_append;Open_creat] 0o666 logfile
   and date = Unix.gmtime (Unix.time ()) in
   output_string out_channel
 		("[" ^ string_of_int (date.Unix.tm_mday)
@@ -30,7 +34,50 @@ let trace message =
 		 ^ ":" ^ string_of_int (date.Unix.tm_min)
 		 ^ ":" ^ string_of_int (date.Unix.tm_sec)
 		 ^ "]" ^ " \"" ^ message ^ "\"\n");
+  if (!verbose_mode) then
+    print_endline message;
   close_out out_channel;;
+
+let init_dict () =
+  let lines =
+    let n = ref 0
+    and in_channel = open_in !dictionary_filename in
+    let rec loop () =
+      ignore (input_line in_channel); incr n; loop ()
+    in
+    try loop () with End_of_file -> close_in in_channel; !n in
+  dictionary_words := Array.append !dictionary_words (Array.make lines "");
+  let in_channel = open_in !dictionary_filename and i = ref 0 in
+  let rec loop () =
+    Array.set !dictionary_words !i (input_line in_channel); incr i; loop ()
+  in
+  try loop () with End_of_file -> close_in in_channel;;
+  
+
+class server port n =
+object(self)
+
+  val port_num = port
+  val nb_pending = n
+  val s_descr = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0
+
+  initializer
+  let host = Unix.gethostbyname (Unix.gethostname()) in
+      let h_addr = host.Unix.h_addr_list.(0) in
+      let sock_addr = Unix.ADDR_INET (h_addr, port_num) in
+      Unix.setsockopt s_descr Unix.SO_REUSEADDR true;
+      Unix.bind s_descr sock_addr;
+      Unix.listen s_descr nb_pending;
+      trace ("Server successfully started with parameters : "
+	     ^ string_of_int !max_players ^ " maximum players and "
+	     ^ string_of_int !timeout ^ " seconds of timeout.\n"
+	     ^ "Server is waiting for players connections on port "
+	     ^ string_of_int port ^ " and host/address "
+	     ^ Unix.gethostname() ^ "/"
+	     ^ Unix.string_of_inet_addr h_addr ^ ".");
+      init_dict ();
+
+end;;
   
 let main () =
   begin
@@ -50,7 +97,7 @@ let main () =
     in let usage_msg = "Options availables for iSketch server :"
        in Arg.parse speclist print_endline usage_msg;
   end;
-  Random.self_init();
-  trace "coucou";;
-  
+  Random.self_init();;
+  new server !port 2;;
+
   main ();;
