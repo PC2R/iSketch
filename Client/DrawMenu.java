8d7724f9755d743656e91a9e5156c8d021f110a6
ClaraMuller <clarinette19@hotmail.fr>
2013-12-01 18:37:08 +0100

    Fix disabled and available mode

--- Client/DrawMenu.java
+++ Client/DrawMenu.java
@@ -29,7 +29,7 @@ public class DrawMenu extends JMenuBar
 	private JMenuItem ms5 = new JMenuItem();
 	private JMenuItem ms8 = new JMenuItem();
 	private JMenuItem ms10 = new JMenuItem();
-	
+
 	private JMenuItem mPass = new JMenuItem("Passer le tour");
 	private JMenuItem mCheat = new JMenuItem("Il triche !");
 
@@ -47,7 +47,7 @@ public class DrawMenu extends JMenuBar
 		this.add(mLine);
 		initMenuOption();
 		this.add(mOption);
-		
+
 		this.drawp = dp;
 	}
 
@@ -88,6 +88,15 @@ public class DrawMenu extends JMenuBar
 		mBlack.setBackground(Color.BLACK);
 		mBlack.setPreferredSize(new Dimension(150, 30));
 		mColors.add(mBlack);
+		
+		mYellow.addActionListener(new ColorActionListener(Color.YELLOW, this));
+		mOrange.addActionListener(new ColorActionListener(Color.ORANGE, this));
+		mRed.addActionListener(new ColorActionListener(Color.RED, this));
+		mPink.addActionListener(new ColorActionListener(Color.PINK, this));
+		mViolet.addActionListener(new ColorActionListener(Color.MAGENTA, this));
+		mBlue.addActionListener(new ColorActionListener(Color.BLUE, this));
+		mGreen.addActionListener(new ColorActionListener(Color.GREEN, this));
+		mBlack.addActionListener(new ColorActionListener(Color.BLACK, this));
 	}
 
 	public void initMenuLine()
@@ -105,6 +114,11 @@ public class DrawMenu extends JMenuBar
 		ms10.setPreferredSize(new Dimension(150, 30));
 		ms10.setText("Très large");
 
+		ms2.addActionListener(new SizeActionListener(2, this));
+		ms5.addActionListener(new SizeActionListener(5, this));
+		ms8.addActionListener(new SizeActionListener(8, this));
+		ms10.addActionListener(new SizeActionListener(10, this));
+		
 		mLine.add(ms2);
 		mLine.add(ms5);
 		mLine.add(ms8);
@@ -116,72 +130,70 @@ public class DrawMenu extends JMenuBar
 		mOption.setHorizontalTextPosition(SwingConstants.CENTER);
 		mOption.setVerticalTextPosition(SwingConstants.CENTER);
 		mOption.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
+
 		mPass.setPreferredSize(new Dimension(150, 30));
 		mCheat.setPreferredSize(new Dimension(150, 30));
+
+		mCheat.addActionListener(new OptionActionListener("CHEAT", this));
+		mPass.addActionListener(new OptionActionListener("PASS", this));
 		
 		mOption.add(mPass);
 		mOption.add(mCheat);
 	}
-	
-	
+
+
 	public void setPassiveMode()
 	{
-		mYellow.removeAll();
-		mOrange.removeAll();
-		mRed.removeAll();
-		mPink.removeAll();
-		mViolet.removeAll();
-		mBlue.removeAll();
-		mGreen.removeAll();
-		mBlack.removeAll();
-		ms2.removeAll();
-		ms5.removeAll();
-		ms8.removeAll();
-		ms10.removeAll();
-	
-		mPass.setForeground(Color.WHITE);
-		mPass.removeAll();
-		mCheat.setForeground(Color.BLACK);
-		mCheat.addActionListener(new OptionActionListener("CHEAT", this));
+		mYellow.setEnabled(false);
+		mOrange.setEnabled(false);
+		mRed.setEnabled(false);
+		mPink.setEnabled(false);
+		mViolet.setEnabled(false);
+		mBlue.setEnabled(false);
+		mGreen.setEnabled(false);
+		mBlack.setEnabled(false);
+		ms2.setEnabled(false);
+		ms5.setEnabled(false);
+		ms8.setEnabled(false);
+		ms10.setEnabled(false);
+
+		mPass.setEnabled(false);
+		mCheat.setEnabled(true);
 	}
-	
+
 	public void setActifMode()
 	{
-		mYellow.addActionListener(new ColorActionListener(Color.YELLOW, this));
-		mOrange.addActionListener(new ColorActionListener(Color.ORANGE, this));
-		mRed.addActionListener(new ColorActionListener(Color.RED, this));
-		mPink.addActionListener(new ColorActionListener(Color.PINK, this));
-		mViolet.addActionListener(new ColorActionListener(Color.MAGENTA, this));
-		mBlue.addActionListener(new ColorActionListener(Color.BLUE, this));
-		mGreen.addActionListener(new ColorActionListener(Color.GREEN, this));
-		mBlack.addActionListener(new ColorActionListener(Color.BLACK, this));
-		
-		ms2.addActionListener(new SizeActionListener(2, this));
-		ms5.addActionListener(new SizeActionListener(5, this));
-		ms8.addActionListener(new SizeActionListener(8, this));
-		ms10.addActionListener(new SizeActionListener(10, this));
-		
-		mPass.setForeground(Color.BLACK);
-		mPass.addActionListener(new OptionActionListener("PASS", this));
-		mCheat.setForeground(Color.WHITE);
-		mCheat.removeAll();
+		mYellow.setEnabled(true);
+		mOrange.setEnabled(true);
+		mRed.setEnabled(true);
+		mPink.setEnabled(true);
+		mViolet.setEnabled(true);
+		mBlue.setEnabled(true);
+		mGreen.setEnabled(true);
+		mBlack.setEnabled(true);
+		ms2.setEnabled(true);
+		ms5.setEnabled(true);
+		ms8.setEnabled(true);
+		ms10.setEnabled(true);
+
+		mPass.setEnabled(true);
+		mCheat.setEnabled(false);
 	}
-	
-	
+
+
 	/* COMMAND */
-	
+
 	public void sendPass(){ drawp.sendCommandPass(); }
-	
+
 	public void sendCheat()
-	{
+	{ 
 		drawp.sendCommandCheat();
-		this.mCheat.removeAll();
+		this.mCheat.setEnabled(false);
 		this.mCheat.setForeground(Color.WHITE);
 	}
-	
+
 	public Color getColor() { return this.color; }
-	
+
 	public int getsize() { return this.size; }
 
 	public void setColor(Color c)
@@ -194,7 +206,7 @@ public class DrawMenu extends JMenuBar
 		this.drawp.setDrawColor(c);
 		this.drawp.sendCommandSetColor(red, green, blue);
 	}
-	
+
 	public void setSize(int s)
 	{ 
 		this.size = s;
